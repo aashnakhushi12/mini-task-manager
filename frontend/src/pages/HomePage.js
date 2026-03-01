236f96e2e375a1b9328a38c7dccf19cea43cef74
Khushi Ashna <khushiaashna@gmail.com>
2026-03-01 04:25:14 +0000

    Removed ESLint error

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,9 +1,7 @@
 import React from "react";
 import Layout from "./../components/Layout/Layout";
-import { useAuth } from "../context/auth";
 
 const HomePage = () => {
-  const [auth, setAuth] = useAuth();
   return (
     <Layout title="Signup Page">
       <div className="signup">
